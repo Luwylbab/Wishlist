a4868dc5dccdcf9b8f61ddbde90027926e2873a2
Lucas <luke.babers@icloud.com>
2024-01-05 16:46:08 -0600

    Adds config, controllers, utils, and updates the server and db files

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-const express = require('express');
 const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 
-const api = require('./routes/index');
-
-const PORT = 3001;
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-
+const PORT = process.env.PORT || 3001;
 
 
 app.listen(PORT, () => {
