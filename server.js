344cfe3776354be510fd4d53bd12ee5538124fc5
SevenImm <angelcardenasptx@gmail.com>
2024-01-06 14:16:56 -0600

    Added require handlebars

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
+const exphbs = require('express-handlebars')
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
@@ -14,3 +15,8 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
 });
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
